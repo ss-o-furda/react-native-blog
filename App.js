063af643858cc604d1ea9113a1011fae5a5f7dee
Orest Furda <orest.f@code-care.pro>
2021-01-22 20:33:44 +0200

    start adding navigation drawer | want to update expo

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
 import AppLoading from "expo-app-loading";
 import { bootstrap } from "./src/bootstrap";
 import { AppNavigation } from "./src/navigation/AppNavigation";
--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -3,12 +3,15 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+import { createDrawerNavigator } from "react-navigation-drawer";
 import { Platform } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { MainScreen } from "../screens/MainScreen";
 import { PostScreen } from "../screens/PostScreen";
 import { THEME } from "../Theme";
 import { BookedScreen } from "../screens/BookedScreen";
+import { AboutScreen } from "../screens/AboutScreen";
+import { CreateScreen } from "../screens/CreateScreen";
 
 const navigatorOptions = {
   defaultNavigationOptions: {
@@ -71,4 +74,16 @@ const BottomNavigator =
         },
       });
 
-export const AppNavigation = createAppContainer(BottomNavigator);
+const MainNavigator = createDrawerNavigator({
+  PostTabs: {
+    screen: BottomNavigator,
+  },
+  About: {
+    screen: AboutScreen,
+  },
+  Create: {
+    screen: CreateScreen,
+  },
+});
+
+export const AppNavigation = createAppContainer(MainNavigator);
